2f40e9fc40a8723415eabb221d342282f524230c
Otto Vehvilainen <otto@mumakil.fi>
2017-09-06 21:00:06 +0300

    Add jwt token model

---
+++ app/models/token.rb
@@ -0,0 +1,47 @@
+require 'jwt'
+
+class Token
+  class InvalidToken < RuntimeError; end
+
+  HMAC_ALGORITHM = 'HS256'.freeze
+
+  def initialize(options)
+    @user = options[:user]
+    @user_id = options[:user_id]
+  end
+
+  def payload
+    now = Time.now.to_i
+    { subject: user.id, iat: now, nbf: now }
+  end
+
+  def user
+    @user || User.find(@user_id)
+  end
+
+  def user_id
+    @user_id || @user.id
+  end
+
+  def encode
+    JWT.encode payload, self.class.secret, HMAC_ALGORITHM
+  end
+
+  def self.decode(token)
+    data, _ = JWT.decode(
+      token,
+      secret,
+      true,
+      algorithm: HMAC_ALGORITHM,
+      verify_iat: true,
+      verify_sub: true
+    )
+    Token.new(user_id: data['subject'])
+  rescue JWT::DecodeError => e
+    raise InvalidToken, e
+  end
+
+  def self.secret
+    Rails.application.secrets.secret_key_base
+  end
+end
---
+++ spec/models/token_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Token, type: :model do
+  describe 'encoding and decoding' do
+    let(:user) { create(:user) }
+    let(:token) { Token.new(user: user) }
+
+    it 'encodes into jwt and decodes' do
+      encoded = token.encode
+      expect(encoded).to be_a(String)
+
+      decoded = Token.decode(encoded)
+      expect(decoded.user_id).to be user.id
+    end
+  end
+end
